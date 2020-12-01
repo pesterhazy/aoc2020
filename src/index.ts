0878b7399fa6e869fb9fa0ecdf2ec2ed191a685a
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-01 20:56:09 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { slurp } from "./util.ts";
 function solvea(xs: number[]) {
   for (var i = 0; i < xs.length; i++) {
     for (var j = 0; j < xs.length; j++) {
-      if (j === i) break;
+      if (j === i) continue;
       if (xs[i] + xs[j] === 2020) {
         console.log(xs[i] * xs[j]);
         return;
@@ -16,7 +16,7 @@ function solveb(xs: number[]) {
   for (var i = 0; i < xs.length; i++) {
     for (var j = 0; j < xs.length; j++) {
       for (var k = 0; k < xs.length; k++) {
-        if (j === i || k === j || i === k) break;
+        if (j === i || k === j || i === k) continue;
         if (xs[i] + xs[j] + xs[k] === 2020) {
           console.log(xs[i] * xs[j] * xs[k]);
           return;
