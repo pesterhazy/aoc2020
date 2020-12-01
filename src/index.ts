05b53a3b2698e6c1f4998c027bb09e734ad88fab
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-01 07:57:50 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
 // todo: SPC j j
 // todo: github repo
 
-async function run() {
-  var text = (await Deno.readTextFile("data/day00.txt")).trimEnd();
+function solvea(xs: number[]) {
+  for (var i = 0; i < xs.length; i++) {
+    for (var j = 0; j < xs.length; j++) {
+      if (j === i) break;
+      if (xs[i] + xs[j] === 2020) {
+        console.log(xs[i] * xs[j]);
+        return;
+      }
+    }
+  }
+}
 
-  console.log(text);
+function solveb(xs: number[]) {
+  for (var i = 0; i < xs.length; i++) {
+    for (var j = 0; j < xs.length; j++) {
+      for (var k = 0; k < xs.length; k++) {
+        if (j === i || k === j || i === k) break;
+        if (xs[i] + xs[j] + xs[k] === 2020) {
+          console.log(xs[i] * xs[j] * xs[k]);
+          return;
+        }
+      }
+    }
+  }
+}
+
+async function run() {
+  var text: string = (await Deno.readTextFile("data/day01a.txt")).trimEnd();
+  var xs = text.split(/\n/).map(n => parseInt(n));
+
+  console.log(xs);
+
+  solvea(xs);
+  solveb(xs);
 }
 
 run();
