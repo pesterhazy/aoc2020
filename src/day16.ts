8b7c341f1935cae2ca8decc073a666acb37c2057
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-16 10:53:50 +0100

    WIP

--- src/day16.ts
+++ src/day16.ts
@@ -1,13 +1,38 @@
 import { slurp } from "./util";
 
 interface World {
-  rules: Map<string, Array<{ lo: number; hi: number }>>;
+  rules: Map<string, { lo: number; hi: number }[]>;
+  mine: number[];
+  nearby: number[];
 }
 
 function parse(s: string): World {
   let chunks = s.split(/\n\n/);
   console.log(chunks);
-  throw new Error(":(");
+  let rules = new Map(
+    chunks[0].split(/\n/).map(l => {
+      let xs = l.split(/\W+/);
+      return [
+        xs[0],
+        [
+          { lo: parseInt(xs[1]), hi: parseInt(xs[2]) },
+          { lo: parseInt(xs[4]), hi: parseInt(xs[5]) }
+        ]
+      ];
+    })
+  );
+  console.log(rules);
+  return {
+    rules: rules,
+    mine: chunks[1]
+      .split(/\n/)[1]
+      .split(/,/)
+      .map(n => parseInt(n)),
+    nearby: chunks[2]
+      .split(/\n/)[1]
+      .split(/,/)
+      .map(n => parseInt(n))
+  };
 }
 
 function solvea(world: World) {
@@ -15,7 +40,7 @@ function solvea(world: World) {
 }
 
 export async function run() {
-  var text: string = await slurp("data/day15.txt");
+  var text: string = await slurp("data/day16.txt");
   var world = parse(text);
 
   solvea(world);
