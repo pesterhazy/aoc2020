916f03d69550ffb7e355bb23b597fd3e538898b1
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-16 11:48:55 +0100

    WIP

--- src/day16.ts
+++ src/day16.ts
@@ -10,11 +10,12 @@ function parse(s: string): World {
   let chunks = s.split(/\n\n/);
   let rules = new Map(
     chunks[0].split(/\n/).map(l => {
-      let xs = l.split(/\W+/);
+      let xs = l.match(/^(.+): (\d+)-(\d+) or (\d+)-(\d+)$/);
+      if (!xs) throw "boom";
       return [
-        xs[0],
+        xs[1],
         [
-          { lo: parseInt(xs[1]), hi: parseInt(xs[2]) },
+          { lo: parseInt(xs[2]), hi: parseInt(xs[3]) },
           { lo: parseInt(xs[4]), hi: parseInt(xs[5]) }
         ]
       ];
@@ -51,6 +52,8 @@ function solvea(world: World) {
 }
 
 function solveb(world: World) {
+  const isValid = (n: number, rule: { lo: number; hi: number }) =>
+    n >= rule.lo && n <= rule.hi;
   console.log(world.rules);
   let tickets: number[][] = [];
   for (let ticket of world.nearby) {
@@ -71,11 +74,56 @@ function solveb(world: World) {
 
     if (ok) tickets.push(ticket);
   }
-  console.log(tickets);
+  let candidates: Map<string, Set<number>> = new Map();
+
+  for (let id of world.rules.keys()) {
+    candidates.set(
+      id,
+      new Set(Array.from({ length: tickets[0].length }, (_, i) => i))
+    );
+  }
+
+  for (let ticket of tickets) {
+    for (let [idx, n] of ticket.entries()) {
+      for (let [id, pair] of world.rules.entries()) {
+        if (!isValid(n, pair[0]) && !isValid(n, pair[1]))
+          candidates.get(id)!.delete(idx);
+      }
+    }
+  }
+  let m: Map<string, number> = new Map();
+  while (true) {
+    let done = true;
+    let progress = false;
+    for (let [id, options] of candidates) {
+      if (options.size === 1) {
+        let idx = [...options][0];
+
+        m.set(id, idx);
+        for (let options of candidates.values()) options.delete(idx);
+        done = false;
+        progress = true;
+        break;
+      }
+      if (options.size > 1) {
+        done = false;
+      }
+    }
+    if (done) break;
+    if (!progress) throw "no progress";
+  }
+  console.log("m", m);
+  let sum = 0;
+  for (let [id, idx] of m) {
+    if (!id.startsWith("departure")) continue;
+
+    sum += world.mine[idx];
+  }
+  console.log("ans", sum);
 }
 
 export async function run() {
-  var text: string = await slurp("data/day16x.txt");
+  var text: string = await slurp("data/day16a.txt");
   var world = parse(text);
 
   solveb(world);
