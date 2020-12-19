c952eef9412b366c3492ad17c285113681a6df98
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-19 14:02:51 +0100

    WIP

--- src/day19.ts
+++ src/day19.ts
@@ -13,8 +13,10 @@ interface AltRule {
 
 type Rule = CharRule | AltRule;
 
+type Ruleset = Map<number, Rule>;
+
 interface World {
-  rules: Map<number, Rule>;
+  rules: Ruleset;
   samples: string[];
 }
 
@@ -42,59 +44,16 @@ function parse(s: string): World {
   return { rules, samples };
 }
 
-function matches(world: World, s: string, ridx: number): number[] {
-  function matchesList(s: string, ridxs: number[]): number[] {
-    if (ridxs.length === 0) return [0];
-
-    let result = [];
-    for (let pos of matches(world, s, ridxs[0])) {
-      for (let len of matchesList(s.substring(1), ridxs.slice(1)))
-        result.push(pos + len);
-    }
-    return result;
-  }
-
-  let rule = world.rules.get(ridx);
-  if (!rule) {
-    throw "Not found";
-  }
-  if (rule.type === "char") {
-    if (s[0] === rule.char) return [1];
-    else return [];
-  } else {
-    let result: number[] = [];
-    for (let alt of rule.alts) {
-      result = [...result, ...matchesList(s, alt)];
-    }
-    return result;
-  }
-}
-
-function solvea(world: World) {
-  // console.log(inspect(world, { showHidden: false, depth: null }));
-  let valid = 0;
-  for (let [idx, sample] of world.samples.entries()) {
-    if (idx !== 2) continue;
-    console.log(sample);
-    let r = matches(world, sample, 0);
-    if (r.length > 0) {
-      console.log("YES", r, sample.length);
-      valid++;
-    } else {
-      console.log("NO", r, sample.length);
-    }
-  }
-  console.log(valid);
-}
-
 function patch(text: string): string {
   return text
     .replace(/^8:.*$/m, "8: 42 | 42 8")
     .replace(/^11:.*$/m, "11: 42 31 | 42 11 31");
 }
 
+function matches(ruleset: Ruleset, s: string): boolean {
+  return false;
+}
+
 export async function run() {
-  var text: string = await slurp("data/day19x.txt");
-  // solvea(parse(text));
-  solvea(parse(patch(text)));
+  var text: string = patch(await slurp("data/day19x.txt"));
 }
