d87a4a8456e593d9cb29405dc8d373ae48b2b813
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-20 13:42:38 +0100

    WIP

--- src/day20.ts
+++ src/day20.ts
@@ -1,5 +1,9 @@
 import { slurp } from "./util";
 
+function dump(o: any) {
+  console.log(JSON.stringify(o, null, 4));
+}
+
 type World = [number, Grid][];
 
 type Grid = string[];
@@ -24,8 +28,50 @@ function parse(s: string): World {
   });
 }
 
+// E, N, W, S
+
+function hash(grid: Grid): string[] {
+  let r: string[] = [];
+  {
+    let s = "";
+    for (let y = 0; y < height(grid); y++) {
+      s += grid[y][width(grid) - 1];
+    }
+    r.push(s);
+  }
+  r.push(grid[0]);
+
+  {
+    let s = "";
+    for (let y = 0; y < height(grid); y++) {
+      s += grid[y][0];
+    }
+    r.push(s);
+  }
+  r.push(grid[height(grid) - 1]);
+  return r;
+}
+
+function reverse(s: string) {
+  let r = "";
+  for (let i = s.length - 1; i >= 0; i--) r += s[i];
+  return r;
+}
+
 function solve(world: World) {
-  console.log(world);
+  let m: Map<string, [number, number][]> = new Map();
+  for (let [n, grid] of world.entries()) {
+    let grid = world[n][1];
+    dump(grid);
+    let hashes = hash(grid);
+    let sigs = [...hashes, ...hashes.map(reverse)];
+    dump(sigs);
+    for (let [idx, sig] of sigs.entries()) {
+      let v = m.get(sig) || [];
+      m.set(sig, [...v, [n, idx]]);
+    }
+  }
+  console.log(m);
 }
 
 export async function run() {
