aef03eca906bce4f2f72c483d1bb001dd01dde0e
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-23 18:27:17 +0100

    WIP

--- src/day22.ts
+++ src/day22.ts
@@ -6,11 +6,32 @@ const test = baretest("aoc");
 type World = number[][];
 
 function parse(s: string): World {
-  console.log(s);
-  throw "not implemented";
+  return s.split(/\n\n/).map(l =>
+    l
+      .split(/\n/)
+      .slice(1)
+      .map(s => parseInt(s))
+  );
 }
 
-function solve(world: World) {}
+function next(stacks: World): World {
+  let a = stacks[0][0];
+  let b = stacks[1][0];
+  let newStacks: World;
+  if (a > b) {
+    // first player wins
+    newStacks = [[...stacks[0].slice(1), a, b], stacks[1].slice(1)];
+  } else {
+    newStacks = [stacks[0].slice(1), [...stacks[1].slice(1), b, a]];
+  }
+  return newStacks;
+}
+
+function solve(world: World) {
+  console.log(world);
+  world = next(world);
+  console.log(world);
+}
 
 export async function run() {
   var text: string = await slurp("data/day22.txt");
