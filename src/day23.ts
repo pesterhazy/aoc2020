ed858c49ca6dd4a4bdb1041497931d3b5aad95f8
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-23 20:06:29 +0100

    WIP

--- src/day23.ts
+++ src/day23.ts
@@ -3,8 +3,6 @@ import { strict as assert } from "assert";
 import baretest from "baretest";
 const test = baretest("aoc");
 
-const N = 9;
-
 const mod1 = (x: number, n: number) => mod(x - 1, n) + 1;
 
 function pick(xs: number[], start: number, len: number): number[] {
@@ -18,27 +16,24 @@ function pick(xs: number[], start: number, len: number): number[] {
   return r;
 }
 
-const findDec = (xs: number[], n: number) => {
-  let dest;
-  while (true) {
-    dest = xs.indexOf(n);
-    if (dest !== -1) return dest;
-    n = mod1(n - 1, N);
-  }
-};
-
 function next(xs: number[]): number[] {
-  assert.equal(9, xs.length);
-  let t = pick(xs, 4, 6);
-  assert.equal(t.length, 6, "t should have 6 items");
-  let destIdx = findDec(t, mod1(xs[0] - 1, N));
+  const findDec = (ns: number[], n: number) => {
+    let dest;
+    while (true) {
+      dest = ns.indexOf(n);
+      if (dest !== -1) return dest;
+      n = mod1(n - 1, xs.length);
+    }
+  };
+  let t = pick(xs, 4, xs.length - 3);
+  let destIdx = findDec(t, mod1(xs[0] - 1, xs.length));
   let pickUp = pick(xs, 1, 3);
   let newXs = [
     ...pick(t, 0, destIdx + 1),
     ...pickUp,
-    ...pick(t, destIdx + 1, 9 - (destIdx + 4))
+    ...pick(t, destIdx + 1, xs.length - (destIdx + 4))
   ];
-  assert.equal(newXs.length, 9, "newXs must have the same length");
+  assert.equal(newXs.length, xs.length, "newXs must have the same length");
   return newXs;
 }
 
@@ -59,8 +54,6 @@ function solve(xs: number[]) {
 const explode = (text: string) => [...text].map(s => parseInt(s));
 
 export async function run() {
-  await test.run();
-
   // let text = "389125467";
   assert.equal(solve(explode("389125467")), "67384529");
 }
