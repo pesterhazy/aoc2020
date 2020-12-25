f64d37552145c4b55168c5454856c89998454bb6
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-25 23:34:25 +0100

    WIP

--- src/day23.ts
+++ src/day23.ts
@@ -2,70 +2,77 @@ import { slurp, mod } from "./util";
 import { strict as assert } from "assert";
 import baretest from "baretest";
 import * as crypto from "crypto";
+var Deque = require("double-ended-queue");
+
 const test = baretest("aoc");
 
 const mod1 = (x: number, n: number) => mod(x - 1, n) + 1;
 
-function pick(xs: number[], start: number, len: number): number[] {
-  let r = [];
-  let i = mod(start, xs.length);
-  while (len > 0) {
-    r.push(xs[i]);
-    i = mod(i + 1, xs.length);
-    len--;
-  }
-  return r;
-}
+function solve(
+  init: number[],
+  size: number,
+  nMoves: number,
+  part1: boolean
+): number {
+  let xs: number[] = [];
 
-function next(xs: number[]) {
-  const findDec = (ns: number[], n: number) => {
-    let dest;
-    while (true) {
-      dest = ns.indexOf(n);
-      if (dest !== -1) return dest;
-      n = mod1(n - 1, xs.length);
-    }
+  // fill array
+  for (let i = 0; i < init.length; i++) {
+    xs.push(init[i]);
+  }
+  for (let i = init.length; i < size; i++) {
+    xs.push(i + 1);
+  }
+
+  // build linked list
+  let next: Map<number, number> = new Map();
+  const getNext = (n: number): number => {
+    let r = next.get(n);
+    if (r === undefined) throw "not found";
+    return r;
   };
-  let t = pick(xs, 4, xs.length - 3);
-  let destIdx = findDec(t, mod1(xs[0] - 1, xs.length));
-  let pickUp = pick(xs, 1, 3);
-  let newXs = [
-    ...pick(t, 0, destIdx + 1),
-    ...pickUp,
-    ...pick(t, destIdx + 1, xs.length - (destIdx + 4))
-  ];
-  assert.equal(newXs.length, xs.length, "newXs must have the same length");
-  for (let i = 0; i < newXs.length; i++) xs[i] = newXs[i];
-}
 
-function answer(xs: number[]) {
-  let idx = xs.indexOf(1);
-  assert.notEqual(idx, -1);
-
-  return pick(xs, idx + 1, 8).join("");
-}
-
-function fill(xs: number[], n: number) {
-  let r = [];
-
-  for (let i = 0; i < xs.length; i++) r.push(xs[i]);
-  for (let i = xs.length; i < n; i++) r.push(i + 1);
-  return r;
-}
-
-function solve(xs: number[], nMoves: number) {
-  for (let i = 0; i < nMoves; i++) {
-    if (i % 100 === 0) console.log("...", i);
-    next(xs);
+  for (let i = 0; i < size; i++) {
+    next.set(xs[i], xs[(i + 1) % size]);
   }
-  return answer(xs);
+
+  let cur = xs[0];
+  for (let move = 0; move < nMoves; move++) {
+    let pickup = getNext(cur);
+    if (pickup === undefined) throw "not found";
+
+    // remove pickups
+
+    next.set(cur, getNext(getNext(getNext(pickup))));
+
+    let dest = mod1(cur - 1, size);
+    let abc = [pickup, getNext(pickup), getNext(getNext(pickup))];
+    while (abc.indexOf(dest) !== -1) dest = mod1(dest - 1, size);
+
+    next.set(abc[2], getNext(dest));
+    next.set(dest, pickup);
+    cur = getNext(cur);
+  }
+
+  // p1
+  if (part1) {
+    let ans = [];
+    let x = getNext(1);
+    while (x !== 1) {
+      ans.push(x);
+      x = getNext(x);
+    }
+    return parseInt(ans.join(""));
+  } else {
+    return getNext(1) * getNext(getNext(1));
+  }
 }
 
 const explode = (text: string) => [...text].map(s => parseInt(s));
 
 export async function run() {
   console.log("*** part1");
-  assert.equal(solve(fill(explode("389125467"), 9), 100), "67384529");
-  // console.log("*** part2");
-  // assert.equal(solve(fill(explode("389125467"), 1000000), 10000000), "xxx");
+  assert.equal(solve(explode("739862541"), 9, 100, true), 94238657);
+  console.log("*** part2");
+  assert.equal(solve(explode("739862541"), 1000000, 10000000, false), -1);
 }
