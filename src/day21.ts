ef0332cd9c7a5281c55f5e92a069f322e44f3603
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-26 00:19:17 +0100

    WIP

--- src/day21.ts
+++ src/day21.ts
@@ -5,12 +5,12 @@ type Inf = [string[], string[]];
 const intersection = (a: any, b: any) => new Set([...a].filter(x => b.has(x)));
 
 function solvea(infs: Inf[]) {
-  console.log(infs);
-
+  let result: Map<string, number> = new Map();
   let assoc: Map<string, string> = new Map();
   let m: Map<string, Array<Set<string>>> = new Map();
 
   for (let [xs, ys] of infs) {
+    for (let x of xs) result.set(x, (result.get(x) || 0) + 1);
     for (let y of ys) {
       let s = m.get(y) || [];
       s.push(new Set(xs));
@@ -24,20 +24,21 @@ function solvea(infs: Inf[]) {
       for (let xs of xss.slice(1)) s = intersection(s, xs);
 
       if (s.size === 1) {
-        let v = s.values().next().value;
-        assoc.set(y, v);
+        let x = s.values().next().value;
+        assoc.set(y, x);
 
         for (let [y, xss] of m.entries()) {
           for (let xs of xss) {
-            xs.delete(v);
+            xs.delete(x);
           }
         }
         m.delete(y);
+        result.delete(x);
         break;
       }
     }
   }
-  console.log(assoc);
+  console.log([...result.values()].reduce((a: number, b: number) => a + b));
 }
 
 function parse(s: string): Inf {
@@ -47,7 +48,7 @@ function parse(s: string): Inf {
 }
 
 export async function run() {
-  var text: string = await slurp("data/day21.txt");
+  var text: string = await slurp("data/day21a.txt");
   var infs = text.split(/\n/).map(parse);
 
   solvea(infs);
