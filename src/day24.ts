da8d566ef7ee33d3476424345be6cbc5bf6d45f9
Paulus Esterhazy <pesterhazy@gmail.com>
2020-12-25 16:18:44 +0100

    WIP

--- src/day24.ts
+++ src/day24.ts
@@ -32,6 +32,51 @@ function count(grid: Grid): number {
   return r;
 }
 
+function bounds(grid: Grid): [vec2, vec2] {
+  let r: [vec2, vec2] = [
+    [Infinity, -Infinity],
+    [Infinity, -Infinity]
+  ];
+  for (let [x, a] of grid)
+    for (let [y, _] of a) {
+      r[0][0] = Math.min(r[0][0], x);
+      r[0][1] = Math.max(r[0][1], x);
+      r[1][0] = Math.min(r[1][0], y);
+      r[1][1] = Math.max(r[1][1], y);
+    }
+
+  return r;
+}
+
+const DELTAS: [number, number][] = [
+  [-1, 0],
+  [0, -1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+  [0, 1]
+];
+
+function next(grid: Grid): Grid {
+  let newGrid = new Map();
+  let b = bounds(grid);
+
+  for (let x = b[0][0] - 1; x <= b[0][1] + 1; x++)
+    for (let y = b[1][0] - 1; y <= b[1][1] + 1; y++) {
+      let neighbors = 0;
+
+      for (let delta of DELTAS) {
+        if (peek(grid, vec2.add(vec2.create(), [x, y], delta))) neighbors++;
+      }
+      let v: boolean = peek(grid, [x, y]) || false;
+      if (v && (neighbors === 0 || neighbors > 2)) v = false;
+      else if (!v && neighbors === 2) v = true;
+      poke(newGrid, [x, y], v);
+    }
+
+  return newGrid;
+}
+
 function solve(tiles: Dir[][]) {
   let grid = new Map();
 
